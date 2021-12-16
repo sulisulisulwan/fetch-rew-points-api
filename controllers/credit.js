dcd117ae4449ee9a2f8efeb65f3ec4c8bbf017e0
Suliman Tekalli <sulimantekalli@gmail.com>
2021-12-16 13:04:06 -0500

    Implement skeleton for credit controller

---
+++ controllers/credit.js
@@ -0,0 +1,14 @@
+const Controller = require('./controller_base_class')
+
+class Credit extends Controller {
+  constructor(models, utils) {
+    super(models, utils)
+  }
+
+  async creditAccount () {
+
+  }
+
+}
+
+module.exports = Credit;
